1a4d89a2217491be38b197a21784659ddedad37e
Neill Hanna <neillhanna@gmail.com>
2015-12-13 23:54:52 -0500

    $(document).ready(function(){
        });

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,11 +1,5 @@
     $(document).ready(function(){
-            $('#navbar-collapse').find('li a').click(function(ev){
-                $('#navbar-collapse').find('li').each(function(){
-                    $(this).removeClass('active');
-                    console.log('clicked each');
-                });
-                $(this).parent().addClass('active');
-                console.log('clicked');
-            });
-    
+        
+        
+        
     });
