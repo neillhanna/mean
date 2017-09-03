e7eecdf2f00a1aec05c5e4b09064420fcb6cdf1a
Neill Hanna <neillhanna@gmail.com>
2017-09-03 02:08:21 -0400

    change to app.js and package.json so will run on openshift

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({
 app.use('/', routes);
 app.use('/users', users);
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
  
 app.listen(server_port, server_ip_address, function () {
